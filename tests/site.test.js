ff245177f285c5f9ae60cd5bc832a1965e192361
keene5 <31411970+keene5@users.noreply.github.com>
2025-02-28 14:27:24 -0500

    Secret Change

--- tests/site.test.js
+++ tests/site.test.js
@@ -3,9 +3,18 @@ test('Check if the homepage loads', () => {
   const secret = process.env.MY_SECRET; // Access the environment variable
   console.log('The secret is:', secret.length);
 
-  expect(secret.length).toBe(11); 
+
     // Here you can use a library like jsdom to simulate the DOM
     // For this example, we can just pass a true condition.
     expect(true).toBe(true);
   });
+
+  test('Check if Secret is Present', () => {
+    const secret = process.env.MY_SECRET; // Access the environment variable
+  
+    expect(secret.length).toBe(11); 
+      // Here you can use a library like jsdom to simulate the DOM
+      // For this example, we can just pass a true condition.
+
+    });
   
