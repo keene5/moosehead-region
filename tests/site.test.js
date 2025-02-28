b4a754068c6d9b7767a800b3f3bf42a9dd890f07
keene5 <31411970+keene5@users.noreply.github.com>
2025-02-28 14:19:39 -0500

    Secret Change

--- tests/site.test.js
+++ tests/site.test.js
@@ -1,6 +1,7 @@
 // tests/example.test.js
 test('Check if the homepage loads', () => {
-  console.log('The secret is:', mySecret);
+  const secret = process.env.MY_SECRET; // Access the environment variable
+  console.log('The secret is:', secret);
     // Here you can use a library like jsdom to simulate the DOM
     // For this example, we can just pass a true condition.
     expect(true).toBe(true);
