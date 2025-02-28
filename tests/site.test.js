1f4a145499c23fb6171879863f81bd3423e2fa08
keene5 <31411970+keene5@users.noreply.github.com>
2025-02-28 13:52:59 -0500

    Pic CHnage

---
+++ tests/site.test.js
@@ -0,0 +1,7 @@
+// tests/example.test.js
+test('Check if the homepage loads', () => {
+    // Here you can use a library like jsdom to simulate the DOM
+    // For this example, we can just pass a true condition.
+    expect(true).toBe(true);
+  });
+  
