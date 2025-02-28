43a132dae48fdf094148572542151334e4ecd04a
keene5 <31411970+keene5@users.noreply.github.com>
2025-02-28 14:14:59 -0500

    Secret Change

--- tests/site.test.js
+++ tests/site.test.js
@@ -1,6 +1,6 @@
 // tests/example.test.js
 test('Check if the homepage loads', () => {
-  console.log('The secret is:', secret);
+  console.log('The secret is:', mySecret);
     // Here you can use a library like jsdom to simulate the DOM
     // For this example, we can just pass a true condition.
     expect(true).toBe(true);
